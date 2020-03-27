58fa4d3f96653d41ccc656c1f32bd40f70bc5e72
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-27 00:00:13 +0000

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 //export const HOST_URL = 'http://104.248.246.254/'
-export const HOST_URL = 'http://0.0.0.0:8000/'
+//export const HOST_URL = 'http://0.0.0.0:8000/'
+export const HOST_URL = 'https://nico.propulsion-learn.ch/'
