7227344981eca0ca65ee7d1df048b234460b3677
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-28 14:17:30 +0000

    Add new file

---
+++ src/components/App/trash/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Login from '../Login/index.js';
+import Feed from '../Feed/index.js';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import HOCWrapper from '../../HOC';
+import store from "../../store";
+import {loginAction} from "../../store/actions/loginAction";
+import User from '../User/index.js'
+
+// fetches the token from the localStorage and saves it in the state
+const token = localStorage.getItem('token');
+const user = localStorage.getItem('user');
+if (token) {
+  store.dispatch(loginAction(token))
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ HOCWrapper(Login) }/> 
+        <Route exact path="/feed" component={ HOCWrapper(Feed) }/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
